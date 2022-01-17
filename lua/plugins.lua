c15cbc0890909079f5d45b5f64b694c7924b5961
Reed Koser <srkoser+GitHub@gmail.com>
2022-01-17 00:00:41 -0800

    add zenbones colorschemes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,6 +14,13 @@ require('packer').startup(function(use)
 
   -- Colorschemes
   use 'rebelot/kanagawa.nvim'
+  use {
+    "mcchrish/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    requires = "rktjmp/lush.nvim"
+  }
 
   -- LuaLine
   use {
